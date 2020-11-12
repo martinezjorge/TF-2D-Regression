06851c4412f355e39e111fe55ade3e214307454c
Jorge Martinez <martinezjorge273@gmail.com>
2020-11-11 23:24:58 -0600

    Added trainModel()

--- script.js
+++ script.js
@@ -33,6 +33,13 @@ async function run() {
     // Create the model
     const model = createModel();
     tfvis.show.modelSummary({name: 'Model Summary'}, model);
+
+    const tensorData = convertToTensor(data);
+    const {inputs, labels} = tensorData;
+
+    // Train the model
+    await trainModel(model, inputs, labels);
+    console.log('Done Training');
 }
 
 function createModel() {
@@ -87,6 +94,29 @@ function convertToTensor(data) {
             labelMax,
             labelMin,
         }
+    });
+}
+
+async function trainModel(model, inputs, labels) {
+    // Prepare the model for training.
+    model.compile({
+        optimizer: tf.train.adam(),
+        loss: tf.losses.meanSquaredError,
+        metrics: ['mse'],
+    });
+
+    const batchSize = 32;
+    const epochs = 50;
+
+    return await model.fit(inputs, labels, {
+        batchSize,
+        epochs,
+        shuffle: true,
+        callbacks: tfvis.show.fitCallbacks(
+            {name: 'Training Performance' },
+            ['loss', 'mse'],
+            { height: 200, callbacks: ['onEpochEnd'] }
+        )
     });
 }
 
